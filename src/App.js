bc97e0d8a362be97497446f9d6bc9c59f1f07129
Pronoy4496 <67531767+Pronoy4496@users.noreply.github.com>
2020-08-23 11:52:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [{name:'Photoshop',price:'$90.99'}]
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My first react paragraph </p>
+        <Counter></Counter>
+        <Product name={products[0].name} price={products[0].price} ></Product>
+       <Person name='Rubel' nayika='Bobita'></Person>
+       <Person name='Jashim' nayika='Shompa'></Person>
+       <Person name='Manna' nayika='Moushumi'></Person>
+       <Person name='Salam' nayika='Shabana'></Person>
       </header>
     </div>
   );
+    function Counter(){
+      const [count, setCount] = useState (0);
+      const handleIncrease = () => setCount(count + 1);
+      
+      return(
+        <div>
+          <h1>Count:0</h1>
+          <button onClick ={handleIncrease}>Increase</button>
+        </div>
+      )
+    }
+
+
+  function Person(props){
+    return (
+    <div style ={{border:"2px solid red", margin: "10px"}}>
+       <h1>Nayok: {props.name}</h1>
+    <h3>Hero of  {props.nayika}</h3>
+    </div>
+    )
+  }
+  function Product(props){
+    const productStyle={
+      border:"1px solid gray",
+      borderRadius: '5px',
+      backgroundColor:'lightgray',
+      height:'200px',
+      width:'200px',
+      float:"left"
+    }
+    
+    return (
+      <div style={productStyle}>
+        <h3>{props.name}</h3>
+        <h5>{props.price}</h5>
+        <button>Buy Now</button>
+      </div>
+    )
+  }
 }
 
 export default App;
